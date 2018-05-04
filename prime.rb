d2f6e6d9a9cde9b9b6d30eea995366444a92f930
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-05-04 15:04:39 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -16,3 +16,17 @@ def prime?(number)
   end
   return true
 end
+
+def prime?(number)
+  if number <= 1 || number == 2
+    return false
+  end
+  n = 2
+  while n < number
+    if number % n == 0 
+      return false
+    end
+    n += 1
+  end
+  return true
+end
